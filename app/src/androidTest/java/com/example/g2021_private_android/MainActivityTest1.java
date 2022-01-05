8501685e704fc1dedf0e99490691ffaac463337a
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-05 18:23:52 +0900

    動かんけど未来の自分への参考までに

---
+++ app/src/androidTest/java/com/example/g2021_private_android/MainActivityTest1.java
@@ -0,0 +1,29 @@
+package com.example.g2021_private_android;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MainActivityTest1 {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void onCreateOptionsMenu() {
+    }
+
+    @Test
+    public void onOptionsItemSelected() {
+    }
+
+    @Test
+    public void onSupportNavigateUp() {
+    }
+}
--- app/src/main/java/com/example/g2021_private_android/MainActivity.java
+++ app/src/main/java/com/example/g2021_private_android/MainActivity.java
@@ -1,72 +1,108 @@
 package com.example.g2021_private_android;
 
+import android.app.Activity;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
 
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
-import com.example.g2021_private_android.databinding.ActivityMainBinding;
+public class MainActivity extends Activity {
 
-public class MainActivity extends AppCompatActivity {
+    // サンプリングレート
+    int SAMPLING_RATE = 44100;
+    // FFTのポイント数
+    int FFT_SIZE = 4096;
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
+    // デシベルベースラインの設定
+    double dB_baseline = Math.pow(2, 15) * FFT_SIZE * Math.sqrt(2);
+
+    // 分解能の計算
+    double resol = ((SAMPLING_RATE / (double) FFT_SIZE));
+
+    AudioRecord audioRec = null;
+    boolean bIsRecording = false;
+    int bufSize;
+    Thread fft;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bufSize = AudioRecord.getMinBufferSize(SAMPLING_RATE,
+                AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
+    }
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
 
-        setSupportActionBar(binding.toolbar);
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // AudioRecordの作成
+        audioRec = new AudioRecord(MediaRecorder.AudioSource.MIC,
+                SAMPLING_RATE, AudioFormat.CHANNEL_IN_MONO,
+                AudioFormat.ENCODING_PCM_16BIT, bufSize * 2);
+        audioRec.startRecording();
+        bIsRecording = true;
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
+        //フーリエ解析スレッド
+        fft = new Thread(new Runnable() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void run() {
+                byte buf[] = new byte[bufSize * 2];
+                while (bIsRecording) {
+                    audioRec.read(buf, 0, buf.length);
+
+                    //エンディアン変換
+                    ByteBuffer bf = ByteBuffer.wrap(buf);
+                    bf.order(ByteOrder.LITTLE_ENDIAN);
+                    short[] s = new short[(int) bufSize];
+                    for (int i = bf.position(); i < bf.capacity() / 2; i++) {
+                        s[i] = bf.getShort();
+                    }
+
+                    //FFTクラスの作成と値の引き渡し
+                    FFT4g fft = new FFT4g(FFT_SIZE);
+                    double[] FFTdata = new double[FFT_SIZE];
+                    for (int i = 0; i < FFT_SIZE; i++) {
+                        FFTdata[i] = (double) s[i];
+                    }
+                    fft.rdft(1, FFTdata);
+
+                    // デシベルの計算
+                    double[] dbfs = new double[FFT_SIZE / 2];
+                    double max_db = -120d;
+                    int max_i = 0;
+                    for (int i = 0; i < FFT_SIZE; i += 2) {
+                        dbfs[i / 2] = (int) (20 * Math.log10(Math.sqrt(Math
+                                .pow(FFTdata[i], 2)
+                                + Math.pow(FFTdata[i + 1], 2)) / dB_baseline));
+                        if (max_db < dbfs[i / 2]) {
+                            max_db = dbfs[i / 2];
+                            max_i = i / 2;
+                        }
+                    }
+
+                    //音量が最大の周波数と，その音量を表示
+                    Log.d("fft","周波数："+ resol * max_i+" [Hz] 音量：" +  max_db+" [dB]");
+                }
+                // 録音停止
+                audioRec.stop();
+                audioRec.release();
             }
         });
+        //スレッドのスタート
+        fft.start();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
 }
